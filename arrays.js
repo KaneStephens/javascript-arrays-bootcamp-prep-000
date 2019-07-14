2e8ce82a83277bc5204022b627e5815112b4b455
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-07-14 15:58:06 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,32 @@
+
+
+var chocolateBars = ["Snickers", "Hundred Grand", "Kitkat", "Skittles"];
+
+function addElementToBeginningOfArray(Array, Element) {
+  var editedArray = [Element, ...Array]
+  return editedArray
+}
+
+function addElementToEndOfArray(Array, Element) {
+  var editedArray = [...Array, Element]
+  return editedArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.shift()
+  var editedArray = [element, ...array]
+  array = editedArray
+  return array
+}
+
+function accessElementInArray(array, index){
+return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  return array.shift()
+}
+
+
+
+
